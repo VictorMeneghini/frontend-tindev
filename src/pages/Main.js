7b81d42f0a7e4a76d27637721c3e4050d9728860
VictorMeneghini <vhmeneghini@gmail.com>
2019-08-08 00:13:00 -0300

    Create schema

---
+++ src/pages/Main.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Main.css';
+
+import api from '../services/api';
+
+import logo from '../assets/tindev.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
+export default function Main({ match }) {
+  const [ users, setUsers ] = useState([]);
+
+  useEffect(() => {
+    async function loadUser() {
+      const response = await api.get('/devs', {
+        headers: { 
+          user: match.params.id,
+        }
+      });
+      setUsers(response.data);
+    }
+    loadUser();
+  }, [ match.params.id ]);
+
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/likes`, null, {
+      headers: {user: match.params.id}
+    });
+    setUsers(users.filter(user => user._id !== id));
+  };
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislikes`, null, {
+      headers: {user: match.params.id}
+    });
+    setUsers(users.filter(user => user._id !== id));
+  };
+
+  return (
+    <div className="main-container">
+      <Link to="/" >
+        <img src={logo} alt="tindev" />  
+      </Link>
+       { users.length > 0 ? (
+         <ul>
+            {users.map( user => (
+              <li key={user._id}>
+                <img src={user.avatar}></img>
+                <footer>
+                  <strong>{user.name}</strong>
+                  <p>{user.bio}</p>
+                </footer>
+                <div className="buttons">
+                  <button 
+                    type="button"
+                    onClick={() => handleLike(user._id)}>
+                    <img src={like} alt="like"></img>
+                  </button>
+                  <button 
+                    type="button"
+                    onClick={() => handleDislike(user._id)}>
+                    <img src={dislike} alt="dislike"></img>
+                  </button>
+              </div>
+            </li>  
+            ))}
+         </ul>
+       ) : (
+         <div className="empty"> acabou ): </div>
+       ) }
+    </div>
+  );
+}
